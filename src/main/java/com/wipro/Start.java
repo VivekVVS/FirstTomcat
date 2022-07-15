d7f307913087a1ab1b16e11b7bf49338c21a8348
vivek <viveksalunke.me@gmail.com>
2022-07-15 08:57:29 +0530

    first commit

---
+++ src/main/java/com/wipro/Start.java
@@ -0,0 +1,11 @@
+package com.wipro;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("We are learning the jenkins");
+
+	}
+
+}
